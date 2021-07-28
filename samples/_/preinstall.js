a8e4611e019fa0791ca20617451654dfbc97bc5f
jaubourg <j@ubourg.net>
2021-07-28 20:51:12 +0200

    samples: has all frameworks draw from the same html source.

---
+++ samples/_/preinstall.js
@@ -0,0 +1,26 @@
+/* globals __dirname, require, process */
+const { readdir, readFile, writeFile } = require( `fs/promises` );
+
+const rTXT = /\.txt$/;
+const rMain = /###/;
+
+const rDependency = /\/node_modules\/@twicpics\/components-sample$/;
+
+// only postintall if a dependency
+if ( !rDependency.test( __dirname ) ) {
+    // eslint-disable-next-line no-console
+    console.error( `cannot install if not a dependency` );
+    process.exit( 1 );
+}
+
+( async () => {
+    const htmlPromise = readFile( `${ __dirname }/Sample.html`, `utf8` );
+    await Promise.all( ( await readdir( `${ __dirname }/templates` ) ).map( async file => {
+        const source = `${ __dirname }/templates/${ file }`;
+        const target = `${ __dirname }/${ file.replace( rTXT, `` ) }`;
+        return writeFile(
+            target,
+            ( await readFile( source, `utf8` ) ).replace( rMain, await htmlPromise )
+        );
+    } ) );
+} )();
