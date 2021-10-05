e875de4a3073090c945c813126049b59e096c611
jaubourg <j@ubourg.net>
2021-10-05 23:04:07 +0200

    vue: fixes noscript.

--- src/vue/base.vue
+++ src/vue/base.vue
@@ -54,7 +54,7 @@ export default {
         :style="_wrapperStyle"
     >
         <component
-            :is="noscript"
+            :is="`noscript`"
             v-if="_noScriptSrc"
         >
             <component
