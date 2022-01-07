5831dae90e6a077ff1d518cf6e874324cc329879
Miguel Beignon <miguel@twic.pics>
2022-01-07 10:05:30 +0100

    Angular directive simplification

--- src/angular/attributes.directive.ts
+++ src/angular/attributes.directive.ts
@@ -5,40 +5,27 @@ import { Directive, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
 } )
 export class AttributesDirective implements OnInit {
 
-  @Input()
-  public twicPicsAttributes?: { [key: string]: string; };
-  // eslint-disable-next-line no-useless-constructor
-  constructor(
-    private renderer: Renderer2,
-    private elementRef: ElementRef
-  ) { }
-  // eslint-disable-next-line class-methods-use-this
-  formatAttributValue( attributeValue: any ): any {
-      let formattedAttributValue = attributeValue;
-      if ( typeof attributeValue === `object` ) {
-          formattedAttributValue = ``;
-          for ( const attributeValueKey in attributeValue ) {
-              formattedAttributValue =
-              `${ formattedAttributValue } ${ attributeValueKey } : ${ attributeValue[ attributeValueKey ] };`;
-          }
-      }
-      return formattedAttributValue;
-  }
-  manageAttributes():void {
-      for ( const attributeName in this.twicPicsAttributes ) {
-          const attributeValue = this.twicPicsAttributes[ attributeName ];
-          if ( attributeValue ) {
-              this.renderer.setAttribute(
-                  this.elementRef.nativeElement,
-                  attributeName,
-                  this.formatAttributValue( attributeValue )
-              );
-          } else {
-              this.renderer.removeAttribute( this.elementRef.nativeElement, attributeName );
-          }
-      }
-  }
-  ngOnInit(): void {
-      this.manageAttributes();
-  }
+    @Input()
+    public twicPicsAttributes?: { [key: string]: string; };
+    // eslint-disable-next-line no-useless-constructor
+    constructor(
+        private renderer: Renderer2,
+        private elementRef: ElementRef
+    ) { }
+    manageAttributes():void {
+        Object.entries( this.twicPicsAttributes ).forEach( ( [ attributName, attributValue ] ) => {
+            if ( attributValue ) {
+                this.renderer.setAttribute(
+                    this.elementRef.nativeElement,
+                    attributName,
+                    attributValue
+                );
+            } else {
+                this.renderer.removeAttribute( this.elementRef.nativeElement, attributName );
+            }
+        } );
+    }
+    ngOnInit(): void {
+        this.manageAttributes();
+    }
 }
