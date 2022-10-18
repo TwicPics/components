a379c00d94e04d953fdb5cbca828845311427fb8
Miguel Beignon <miguel@twic.pics>
2022-10-18 09:42:34 +0200

    chores: renames angular utilities.

---
+++ build/angular/installer.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+import { getAngularDirectories } from "./utils.js";
+import { execSync } from "child_process";
+
+// retrieve the angular directories where the installation should be launched
+const angularDirectories = await getAngularDirectories();
+// loop on angular directories
+for await ( const angularDirectory of angularDirectories ) {
+    console.log( `installing ${ angularDirectory.name } from ${ angularDirectory.path }` );
+    execSync( `cd ${ angularDirectory.path } && $npm_execpath install` );
+    console.log( `${ angularDirectory.name } installation done` );
+}
