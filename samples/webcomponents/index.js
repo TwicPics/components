b97eb79a4bcd0fb6856dd27ec3d1ee5afd849915
Miguel Beignon <miguel@twic.pics>
2022-11-08 19:06:05 +0100

    Modification of the webcomponent samples in order to test the components in a shadow-root element.

--- samples/webcomponents/index.js
+++ samples/webcomponents/index.js
@@ -7,6 +7,28 @@ installTwicPics( {
     "env": `production`,
 } );
 
+class TestContainer extends HTMLElement {
+
+    constructor( ) {
+        super();
+        const shadowRoot = this.attachShadow( {
+            "mode": `open`,
+        } );
+
+        // should be managed by components
+        const styleComponents = document.createElement( `style` );
+        styleComponents.innerHTML = document.getElementById( `styleComponents` ).content.textContent;
+        shadowRoot.appendChild( styleComponents );
+        // end should be managed by components
+
+        const styleSamples = document.createElement( `style` );
+        styleSamples.innerHTML = document.getElementById( `styleSamples` ).content.textContent;
+        shadowRoot.appendChild( styleSamples );
+        shadowRoot.appendChild( document.getElementById( `content` ).content.cloneNode( true ) );
+    }
+}
+
+customElements.define( `test-container`, TestContainer );
 customElements.define( `twic-img`, TwicImg );
 customElements.define( `twic-video`, TwicVideo );
 customElements.define( `twic-view`, TwicView );
