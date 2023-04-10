10b8f12520c1ca4bea71a204cef608b967216a00
Miguel Beignon <miguel@twic.pics>
2023-04-10 18:55:14 +0200

    Adds angular15 sample

---
+++ samples/angular15/src/app/app.component.ts
@@ -0,0 +1,26 @@
+/* eslint-disable class-methods-use-this */
+import { Component } from "@angular/core";
+import { installTwicpics, StateEvent, TwicImgComponent } from "@twicpics/components/angular15";
+@Component( {
+    "selector": `app-root`,
+    "templateUrl": `./app.component.html`,
+} )
+export class AppComponent {
+    onStateChange( stateEvent: StateEvent ) {
+        // eslint-disable-next-line no-console
+        console.log( `Angular - TwicImg emits new state event`, stateEvent );
+        const { state, target } = stateEvent;
+        const _target = target as TwicImgComponent;
+        // eslint-disable-next-line no-console
+        console.log( `TwicComponent emits a new state`, state );
+        // eslint-disable-next-line no-console
+        console.log( `TwicComponent source was`, _target.src );
+    }
+}
+
+installTwicpics( {
+    "domain": `https://demo.twic.pics`,
+    "anticipation": 0.5,
+    "step": 100,
+    "env": `production`,
+} );
