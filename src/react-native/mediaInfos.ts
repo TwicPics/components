8c9a04f1880618214fccd1649ef88164dea17fe6
Miguel Beignon <mbgspcii@gmail.com>
2023-07-19 18:04:27 +0200

    Simplfies LQIP data retrieval by using 'inspect' route in React Native.

--- src/react-native/mediaInfos.ts
+++ src/react-native/mediaInfos.ts
@@ -4,31 +4,6 @@ import type { IMediaData, MediaData, SizeObject } from "./types";
 
 const BLUR_RADIUS = 5;
 
-const COLOR = 3;
-const HEIGHT = 2;
-const URI = 4;
-const WIDTH = 1;
-
-// eslint-disable-next-line max-len
-const rLqipData = /(?:(?:width="([0-9]*)").*(?:height="([0-9]*)").*)(?:(?:background:(#[0-9a-fA-F]*)")|(?:href="([^"]*)))/;
-
-const getPlaceholderData = async (
-    url: string
-): Promise< IMediaData > => {
-    const resp = await fetch( url );
-    const data = await resp.text();
-    const parsed = rLqipData.exec( data );
-    return {
-        "placeholder": {
-            "blurRadius": Platform.OS === `web` ? 0 : BLUR_RADIUS,
-            "color": parsed && parsed[ COLOR ],
-            "uri": Platform.OS === `web` ? url : parsed && parsed[ URI ],
-        },
-        "height": ( parsed && Number( parsed[ HEIGHT ] ) ) || undefined,
-        "width": ( parsed && Number( parsed[ WIDTH ] ) ) || undefined,
-    };
-};
-
 export const getMediaData = async ( srcMedia: string, srcLqip: string, viewSize: SizeObject ): Promise< MediaData > => {
     const success = ( data: IMediaData ): MediaData => {
         const { height, width, placeholder } = data;
@@ -40,7 +15,17 @@ export const getMediaData = async ( srcMedia: string, srcLqip: string, viewSize:
         };
     };
     if ( srcLqip ) {
-        return success( await getPlaceholderData( srcLqip ) );
+        const resp = await fetch( `${ srcLqip }/inspect` );
+        const { "output": { color, image, height, width } } = await resp.json();
+        return success( {
+            "placeholder": {
+                "blurRadius": Platform.OS === `web` ? 0 : BLUR_RADIUS,
+                color,
+                "uri": Platform.OS === `web` ? srcLqip : image,
+            },
+            width,
+            height,
+        } );
     }
     return new Promise( resolve => {
         Image.getSize( srcMedia, ( width, height ) => {
