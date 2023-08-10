2e7fa500e14bd4ae32f6fe971646980ebfff737b
Miguel Beignon <mbgspcii@gmail.com>
2023-08-10 10:15:43 +0200

    Exports types in react.

--- src/react/index.ts
+++ src/react/index.ts
@@ -5,4 +5,5 @@ import TwicInstall from "./TwicInstall";
 import TwicVideo from "./TwicVideo";
 import TwicView from "./TwicView";
 const installTwicpics = installTwicPics;
+export type { Anchor, Environment, Mode, Placeholder, State, StateEvent } from "../_/types";
 export { installTwicpics, installTwicPics, TwicBackground, TwicImg, TwicInstall, TwicVideo, TwicView };
