8076101322cbf4db1d99d2cc8d30fb4bd96099a0
Miguel Beignon <mbgspcii@gmail.com>
2023-12-13 14:36:05 +0100

    Fixes resolutions list generation.

--- src/_/const.ts
+++ src/_/const.ts
@@ -1,10 +1,2 @@
-import { config } from "./config";
-import type { BreakPoint } from "./types";
-
 export const VERSION = `v1`;
 
-export const Resolutions = [ `xs`, `sm`, `md`, `lg`, `xl`, `2xl` ]
-    .map( r => config.breakpoints[ r as BreakPoint ] )
-    .sort( ( a, b ) => a - b );
-
-export const ResolutionMax = Resolutions[ Resolutions.length - 1 ];
