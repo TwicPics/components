a235226e67265259060b538dc958517bb5c90b78
Miguel Beignon <mbgspcii@gmail.com>
2024-01-30 10:37:15 +0100

    Moves fetchPriorityAttributes to utils.ts and simplifications.

--- src/react/utils.ts
+++ src/react/utils.ts
@@ -1,14 +1,23 @@
 import { version } from "react";
 
-const reactVersionFactory = () => {
+/**
+ * in React 18.3.0 or newer, we must use camelCase attribute
+ * to avoid "Warning: Invalid DOM property".
+ * in React 18.2.0 or older, we must use lowercase attribute
+ * to avoid "Warning: Invalid DOM property".
+ * see https://github.com/facebook/react/pull/25927
+ */
+const fetchPriorityAttrFactory = () => {
     const [ _major, _minor ] = version.split( `.` ).map( Number );
     const major = isNaN( _major ) ? 0 : _major;
     const minor = isNaN( _minor ) ? 0 : _minor;
-    return ( {
-        major,
-        minor,
+    // eslint-disable-next-line no-magic-numbers
+    const actual = ( major > 18 ) || ( ( major === 18 ) && ( minor >= 3 ) ) ?
+      `fetchPriority` :
+      `fetchpriority`;
+    return ( fetchPriority: string ): Record<string, string> => ( {
+        [ actual ]: fetchPriority,
     } );
 };
 
-export const reactVersion = reactVersionFactory();
-
+export const fetchPriorityAttr = fetchPriorityAttrFactory();
