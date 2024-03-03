18a090836904facc10ef9a00e73d9de7f4cc2954
Miguel Beignon <mbgspcii@gmail.com>
2024-03-03 22:05:09 +0100

    Upgrades samples dependencies.

--- samples/sveltekit/vite.config.js
+++ samples/sveltekit/vite.config.js
@@ -3,6 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
     "plugins": [ sveltekit() ],
+    "server": {
+      "port": 3000,
+    },
 };
 
 export default config;
