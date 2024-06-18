aaa41d22a467e7ad0f351f7765ac8296fa2fd2b4
Miguel Beignon <mbgspcii@gmail.com>
2024-06-18 19:05:16 +0200

    chores: fixes lint issue.

--- src/react/utils.ts
+++ src/react/utils.ts
@@ -5,8 +5,7 @@ import React from 'react';
  * to avoid "Warning: Invalid DOM property".
  * in React version that does not expose `use`, we must use lowercase attribute
  * to avoid "Warning: Invalid DOM property".
- * 
+ *
  * see https://github.com/vercel/next.js/pull/65235
  */
-// @ts-ignore
 export const fetchPriorityName = React.use ? `fetchPriority` : `fetchpriority`;
