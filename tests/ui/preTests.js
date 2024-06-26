91ee58b66fc8ca51ac3810de867b29d0d9ad2707
Miguel Beignon <mbgspcii@gmail.com>
2024-06-26 11:18:15 +0200

    Starts unit testing projects.

--- tests/ui/preTests.js
+++ tests/ui/preTests.js
@@ -1,6 +1,27 @@
+import { exec, spawn } from "child_process";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import units from "./units.js";
 
+const __dirname = dirname( fileURLToPath( import.meta.url ) );
+
 await Promise.all( units.map( async unit => { 
-  const { framework, port } = unit;
-  console.log("framework", framework);
+    const { framework, port } = unit;
+    console.log( `Starting ${ framework } on port ${ port }.` );
+    await new Promise( ( resolve ) => {
+        exec( `cd ${ __dirname }/${ framework } && $npm_execpath install`, () => {
+            const server = spawn( `$npm_execpath`, [ 'serve', '--port', port ], {
+                cwd: `${ __dirname }/${ framework }`,
+                shell: true,
+                detached: true,
+                stdio: 'ignore'
+            } );
+            console.log(`${ framework } started on http://localhost:${ port }.`);
+            server.unref();
+            resolve();
+        } );
+    } );
 } ) );
+
+console.log( `All servers running.` );
+
