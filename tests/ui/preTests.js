0d07f5bdac96b44e52b873ae55af5b146b55587d
Miguel Beignon <mbgspcii@gmail.com>
2024-06-29 15:51:44 +0200

    Ensures that unit testing server are actually started.

--- tests/ui/preTests.js
+++ tests/ui/preTests.js
@@ -1,10 +1,24 @@
 import { exec, spawn } from "child_process";
+import http from "http";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import units from "./units.js";
 import { killports } from "./utils.js";
 
 const __dirname = dirname( fileURLToPath( import.meta.url ) );
+
+const waitForServer = async ( port ) => {
+    return new Promise( resolve => {
+        const interval = setInterval( () => {
+            http.get( `http://localhost:${ port }`, res => {
+                if ( res.statusCode === 200 ) {
+                  clearInterval( interval );
+                  resolve()
+                }
+            } ).on( 'error', () => { } );
+        }, 1000 ) }
+    );
+}
 
 await killports();
 
@@ -19,9 +33,11 @@ await Promise.all( units.map( async unit => {
                 detached: true,
                 stdio: 'ignore'
             } );
-            console.log(`${ framework } started on http://localhost:${ port }.`);
-            server.unref();
-            resolve();
+            waitForServer( port ).then( () => {
+              console.log(`${ framework } started on http://localhost:${ port }.`);
+              server.unref();
+              resolve();
+            } );
         } );
     } );
 } ) );
