ab6e23d2ee7de73abdd9333698441f1bd26553c3
Miguel Beignon <mbgspcii@gmail.com>
2024-07-01 21:06:22 +0200

    Improves testing performance by allowing parallelism with a max of 3 threads.

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig( {
     "test": {
         "environment": `jsdom`,
+        "poolOptions": {
+            "threads": {
+                "maxThreads": 3,
+                "minThreads": 1,
+            },
+        },
+        "fileParallelism": true,
     },
 } );
