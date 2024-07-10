542c39adc2daabfa718121e4794b092a64a37c75
Miguel Beignon <mbgspcii@gmail.com>
2024-07-10 22:26:15 +0200

    Fixes Nuxt3 sample and testing projects in MS Window environment.

--- samples/nuxt3/nuxt.config.ts
+++ samples/nuxt3/nuxt.config.ts
@@ -2,6 +2,7 @@
 // eslint-disable-next-line no-undef
 export default defineNuxtConfig( {
     "modules": [ `@twicpics/components/nuxt3` ],
+    "telemetry": false,
     "twicpics": {
         "domain": `https://demo.twic.it`,
         "anticipation": 0.5,
--- tests/ui/nuxt3/nuxt.config.ts
+++ tests/ui/nuxt3/nuxt.config.ts
@@ -3,6 +3,7 @@
 import config from '../_/config';
 export default defineNuxtConfig( {
     "modules": [ `@twicpics/components/nuxt3` ],
+    "telemetry": false,
     "twicpics": config,
     "css": [
       '@/styles.css'
