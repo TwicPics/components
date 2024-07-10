10bd8142657c20de750105a410b22aedf4fddfa6
Miguel Beignon <mbgspcii@gmail.com>
2024-07-10 20:35:09 +0200

    Fixes sample.css filename case.

--- tests/ui/vue3/index.js
+++ tests/ui/vue3/index.js
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import Sample from './Sample.vue';
 import TwicPics from "@twicpics/components/vue3";
 import "@twicpics/components/style.css";
+import config from '../_/config';
 import "../_/sample.css";
-import config from "../_/config";
 
 const app = createApp( Sample );
 app.use( TwicPics, config );
