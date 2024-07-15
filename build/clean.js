c4ac98115e05a55755c760f4ac9dffca57516b86
Miguel Beignon <mbgspcii@gmail.com>
2024-07-15 10:58:46 +0200

    Fixes issue preventing from deleting dependencies in samples or testing projects.

--- build/clean.js
+++ build/clean.js
@@ -1,19 +1,27 @@
 /* eslint-disable no-console */
 import __dirname from "./__dirname.js";
-import { readdir, stat } from "fs/promises";
-import { remove } from "fs-extra";
+import { readdir, rm, stat } from "fs/promises";
 
 const toDelete = new Set( [
     `.angular`, `.cache`, `.expo`, `.next`, `.nuxt`, `.svelte-kit`, `build`, `dist`, `node_modules`,
     `package-lock.json`, `yarn.lock`,
 ] );
 
+const deleteForce = async path => {
+    try {
+        await rm( path, {
+            "recursive": true,
+            "force": true,
+        } );
+    } catch { }
+};
+
 const clean = async dir => Promise.all(
     ( await readdir( dir ) ).map( file => {
         const path = `${ dir }/${ file }`;
         return (
             toDelete.has( file ) ?
-                remove( path ) :
+                deleteForce( path ) :
                 stat( path ).then( s => ( s.isDirectory() && clean( path ) ) )
         );
     } )
@@ -25,3 +33,4 @@ export default async () => {
     console.log( `cleaning dependencies in tests subdirectory...` );
     await clean( `${ __dirname }/../tests` );
 };
+
