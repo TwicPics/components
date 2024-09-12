6eaa8dc6b102a5b38842afabc66468a98fac7b0b
Miguel Beignon <mbgspcii@gmail.com>
2024-09-12 15:16:40 +0200

    Enables passing object arguments to methods in Vue computed properties.

--- src/vue/utils.ts
+++ src/vue/utils.ts
@@ -1,13 +1,32 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const rInitialProps = /^(\*+)(.*[^*])(\*+)$/;
-export const callFactory = ( func: ( ...p:any[] ) => any, _args: any[] ) => {
-    const args = _args.map( arg => ( ( typeof arg === `function` ) ? {
-        "f": arg,
-    } : {
-        // eslint-disable-next-line no-nested-ternary
-        "s": ( rInitialProps.test( `${ arg }` ) ? `${ `${ arg }`.replace( rInitialProps, `$2` ) }` : `p_${ arg }` ),
-    } ) );
-    return ( context: unknown ) => func( ...args.map(
-        ( { f, s } ) => ( f ? f( context ) : ( context as Record< string, any> )[ s ] )
+const actualPropName = ( prop: string ) =>
+    ( rInitialProps.test( prop ) ? prop.replace( rInitialProps, `$2` ) : `p_${ prop }` );
+export const callFactory = ( func: ( ...p: any[] ) => any, _args: any[] ) => {
+    const args = _args.map( arg => (
+        {
+            "f": ( typeof arg === `function` ) && arg,
+            "o": Array.isArray( arg ) && arg.map( prop => [ prop, actualPropName( prop ) ] ),
+            "s": ( typeof arg === `string` ) && actualPropName( arg ),
+        }
     ) );
+    return ( context: unknown ) => func(
+        ...args.map( ( { f, o, s } ) => {
+            if ( f ) {
+                return f( context );
+            }
+            if ( o ) {
+                return Object.fromEntries(
+                    o.map( ( [ param, propName ] ) =>
+                        [ param, ( context as Record<string, any> )[ propName ] ] )
+                );
+            }
+            if ( s ) {
+                return ( context as Record<string, any> )[ s ];
+            }
+            return undefined;
+        } )
+    );
 };
+
