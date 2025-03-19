e9415600237d6be6a671323616c78dc34781378b
Miguel Beignon <mbgspcii@gmail.com>
2025-03-19 14:19:27 +0100

    Fix brand configuration replacers alphabetical order.

--- build/brand.js
+++ build/brand.js
@@ -1,11 +1,11 @@
 export const replacersConfiguration = ( brand = `twicpics` ) => [
-    [ /\bBRAND[^:]/g, `'${ brand }'` ],
-    [ /\b__INSTALL_FUNCTION__/g, `${ brand === `ffy` ? `installFrontify` : `installTwicpics` }` ],
     [ /\b__BACKGROUND_COMPONENT__/g, `${ brand === `ffy` ? `FfyBackground` : `TwicBackground` }` ],
     [ /\b__IMG_COMPONENT__/g, `${ brand === `ffy` ? `FfyImg` : `TwicImg` }` ],
     [ /\b__INSTALL_COMPONENT__/g, `${ brand === `ffy` ? `FfyInstall` : `TwicInstall` }` ],
+    [ /\b__INSTALL_FUNCTION__/g, `${ brand === `ffy` ? `installFrontify` : `installTwicpics` }` ],
     [ /\b__NG_MODULE__/g, `${ brand === `ffy` ? `FfyComponentsModule` : `TwicPicsComponentsModule` }` ],
     [ /\b__PICTURE_COMPONENT__/g, `${ brand === `ffy` ? `FfyPicture` : `TwicPicture` }` ],
     [ /\b__VIDEO_COMPONENT__/g, `${ brand === `ffy` ? `FfyVideo` : `TwicVideo` }` ],
     [ /\b__VIEW_COMPONENT__/g, `${ brand === `ffy` ? `FfyView` : `TwicView` }` ],
+    [ /\bBRAND[^:]/g, `'${ brand }'` ],
 ];
