e73e818b0a801ee13785b4f9920b5b648e8bae87
Miguel Beignon <mbgspcii@gmail.com>
2025-03-20 17:05:50 +0100

    Adds support to Angular 19

--- build/angular/config.js
+++ build/angular/config.js
@@ -3,7 +3,7 @@ export default {
     // built versions
     "build": [ 11, 12 ],
     "from": 11,
-    "to": 18,
+    "to": 19,
     // versions that do not support directory import
-    "noDirectoryImport": [ 17, 18 ],
+    "noDirectoryImport": [ 17, 18, 19 ],
 };
